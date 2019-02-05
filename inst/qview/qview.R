ddc55bc8306e2648d808cbd571c9f20685d86e11
John Carlo Salter <juuncaerlum@gmail.com>
2019-02-05 17:02:50 -0500

    add: Sample implementation without module syntax.

--- inst/qview/qview.R
+++ inst/qview/qview.R
@@ -1,3 +1,8 @@
+library(shiny)
+library(tidyverse)
+library(DT)
+
+
 #' queueViewOutput
 #' @export
 queueViewOutput <- function(id) {
@@ -5,18 +10,29 @@ queueViewOutput <- function(id) {
   tableOutput(ns("queue"))
 }
 
+#' #' queueButtonInput
+#' #' @export
+#' queueButtonInput <- function(id, label, job_id) {
+#'   ns <- NS(id)
+#'   actionButton(
+#'     inputId = ns(id),
+#'     label = label,
+#'     icon = icon("stop-circle"),
+#'     job_id = job_id
+#'   )
+#' }
 
 #' queueView
 #' @export
-queueView <- function(input, output, session) {
+queueView <- function(input, output, session, queue_obj = rrsq::RSimpleQueue$new()) {
   output$queue <- renderTable(
     {
       invalidateLater(1000, session)
-      queueObj <- rrsq::RSimpleQueue$new()
-      # Check null, error, etc.
+
+      queueViewFrame <- rrsq::jobs_to_df(queue_obj$get_jobs())
 
       return(
-        rrsq::jobs_to_df(queueObj$get_jobs())
+        queueViewFrame
       )
     }
   )
@@ -25,12 +41,52 @@ queueView <- function(input, output, session) {
 
 ## Testing ####
 
+action_button_creator <- function(index) {
+  return(
+    as.character(
+      actionButton(
+        as.character(index),
+        glue::glue("index{index}"),
+        onclick = 'Shiny.onInputChange(\"select_button\",  this.id)'
+      )
+    )
+  )
+}
+
 ui <- fluidPage(
-  queueViewOutput("q")
+  queueViewOutput("q"),
+  DT::dataTableOutput("test_table"),
+  textOutput("cancel_confirmation")
 )
 
 server <- function(input, output, session) {
-  whatDoesThisVariableDo <- callModule(queueView, "q")
+  # whatDoesThisVariableDo <- callModule(queueView, "q", queue_obj = rrsq::RSimpleQueue$new())
+
+  queue_obj = rrsq::RSimpleQueue$new()
+
+  queue_data <- reactive({
+    #tibble(jobID = c("asdfasdf", "weqwerqwerqwer"))
+    rrsq::jobs_to_df(queue_obj$get_jobs())
+  })
+
+  output$test_table  <- DT::renderDT(
+    {
+      queue_data() %>% mutate(button = map_chr(1:n(), action_button_creator))
+    },
+    escape = FALSE
+  )
+
+  selected_index <- eventReactive(input$select_button, {
+    input$select_button
+  })
+
+  output$cancel_confirmation <- renderText({
+    index <- as.numeric(selected_index())
+    selected_row <- queue_data() %>% slice(index)
+    return(glue::glue("cancelling: {selected_row$id}"))
+  })
+
+
 }
 
 shinyApp(ui = ui, server = server)
