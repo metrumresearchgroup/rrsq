8c068b40893b4686ae0080260ab68bf7fe5ec307
Devin Pastoor <devin.pastoor@gmail.com>
2019-03-10 16:33:37 -0400

    test: add additional queue env var test
    References #14

--- tests/testthat/test-queue.R
+++ tests/testthat/test-queue.R
@@ -1,5 +1,19 @@
 context("test-queue")
 
+poll_queue <- function(rsq, .id) {
+  jr <- rsq$get_job(.id)
+  ..times <- 1
+  while(!(jr$status %in% c("COMPLETED", "ERROR"))){
+    if (..times < 5) {
+      ..times <- ..times + 1
+      jr <- rsq$get_job(.id)
+    } else {
+      stop("failed retrieving job submission, timed out after 5 attempts")
+    }
+    Sys.sleep(0.2)
+  }
+  jr
+}
 
 describe("submitting jobs to real queue", {
   skip_if_no_rsq()
@@ -21,20 +35,29 @@ describe("submitting jobs to real queue", {
 
     # will take at least 150 ms or so to start up R and run the external script
     Sys.sleep(0.25)
-    jr <- rsq$get_job(j1$id)
-
-    ..times <- 1
-    while(!(jr$status %in% c("COMPLETED", "ERROR"))){
-      if (..times < 5) {
-        ..times <- ..times + 1
-        jr <- rsq$get_job(j1$id)
-      } else {
-       stop("failed retrieving job submission, timed out after 5 attempts")
-      }
-      Sys.sleep(0.2)
-    }
+    jr <- poll_queue(rsq, j1$id)
     expect_equal(jr$status, "COMPLETED")
     expect_equal(jr$result$output, "RRSQTEST1 value: test1")
+    expect_equal(jr$result$exit_code, 0)
+  })
+
+  it("captures ignores non-captured custom environment variables", {
+    withr::with_envvar(c("RRSQTEST1" = "test1"), {
+      j2 <- rsq$submit_job(
+        user = "devin",
+        context = "env",
+        work_dir = getwd(),
+        rscript_path = "test_scripts/test_env.R"
+      )
+    })
+    expect_equal(j1$status, "QUEUED")
+    expect_false("RRSQTEST1" %in% names(j1$rscript$renv))
+
+    # will take at least 150 ms or so to start up R and run the external script
+    Sys.sleep(0.25)
+    jr <- poll_queue(rsq, j2$id)
+    expect_equal(jr$status, "COMPLETED")
+    expect_equal(jr$result$output, "RRSQTEST1 value: ")
     expect_equal(jr$result$exit_code, 0)
   })
 })
