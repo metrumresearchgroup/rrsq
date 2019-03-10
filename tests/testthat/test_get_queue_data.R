d4fab861cacab52afcd5e19153199c1238347262
Devin Pastoor <devin.pastoor@gmail.com>
2019-03-10 17:04:33 -0400

    chore: minor test cleanup

--- tests/testthat/test_get_queue_data.R
+++ tests/testthat/test_get_queue_data.R
@@ -1,9 +1,7 @@
-
 context("Test get_queue_data")
 
-library(rrsq)
 
-testthat::test_that("Returns null when null, no jobs in queue", {
+test_that("Returns null when null, no jobs in queue", {
   queue_obj <- list() # RSimpleQueue$new() # -- If we have to initialize a new RSimpleQueue, tests become dependent on an RSQ running.
   .user <- "Cats_and_Oranges"
   jobs <- dget(file = file.path("test_data/job_list_zero_items.txt"))
@@ -15,10 +13,10 @@ testthat::test_that("Returns null when null, no jobs in queue", {
   )
 
   result <- get_queue_data(queue_obj, .user)
-  testthat::expect_null(result)
+  expect_null(result)
 })
 
-testthat::test_that("Returns null when null, jobs in queue for other users only", {
+test_that("Returns null when null, jobs in queue for other users only", {
   queue_obj <- list() # RSimpleQueue$new()
   .user <- "Cats_and_Oranges"
   jobs <- dget(file = file.path("test_data/job_list_three_items.txt"))
@@ -33,7 +31,7 @@ testthat::test_that("Returns null when null, jobs in queue for other users only"
   testthat::expect_null(result)
 })
 
-testthat::test_that("Returns queue objects when applicable", {
+test_that("Returns queue objects when applicable", {
 
   queue_obj <- list() # RSimpleQueue$new()
   .user = "Professor Pouch" # Should be two entries
