ffe1fd1a12c99c302de17585e6c3abd2c23ce143
Devin Pastoor <devin.pastoor@gmail.com>
2019-03-10 14:41:56 -0400

    test: break test out for more context clarity

--- tests/testthat/test-collect-r-details.R
+++ tests/testthat/test-collect-r-details.R
@@ -6,6 +6,9 @@ DEFAULT_ENV_NAMES <- c(
  "R_LIBS_USER",
  "R_LIBS"
 )
+
+Sys.setenv(RRSQTESTENV1 = "test1", RRSQTESTENV2 = "test2")
+
 describe("collect_r_details handles environment variables", {
   it("sets default R lib paths env variables", {
     renv <- collect_r_details()$renv
@@ -14,10 +17,11 @@ describe("collect_r_details handles environment variables", {
     expect_true(all(purrr::map_lgl(unlist(renv), ~ .x == DEFAULT_LIB_STRING)))
   })
   it("will keep requested env", {
-    Sys.setenv(RRSQTESTENV1 = "test1", RRSQTESTENV2 = "test2")
     renv <- collect_r_details(.envs = c("RRSQTESTENV1", "RRSQTESTENV2"))$renv
     expect_equal(renv$RRSQTESTENV1, "test1")
     expect_equal(renv$RRSQTESTENV2, "test2")
+  })
+  it("will not keep unrequested env", {
     renv <- collect_r_details(.envs = c("RRSQTESTENV1"))$renv
     expect_equal(renv$RRSQTESTENV1, "test1")
     expect_null(renv$RRSQTESTENV2)
