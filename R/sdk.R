d7efd8e7cfeb54ee3f55e03843a126efa2992fbd
John Carlo Salter <juuncaerlum@gmail.com>
2021-08-19 16:57:53 -0400

    add: adding submit_job2 function to submit jobs without an R6 class.

--- R/sdk.R
+++ R/sdk.R
@@ -21,7 +21,8 @@ CONSTANTS <- list(
     SERVER = "rsq_server_error"
   ),
   CLASSES = list(
-    API_CONFIG = "rsq_api_config"
+    API_CONFIG = "rsq_api_config",
+    RSQ_JOB = "rsq_job"
   ),
   JOB_STATUS = list(
     QUEUED = "QUEUED",
@@ -237,7 +238,7 @@ api_post <- function(
   resp <- httr::POST(
     url = endpoint_url,
     body = body,
-    encode = "application/json",
+    encode = "json",
     config = NULL
   )
 
@@ -282,7 +283,7 @@ create_api_config <- function(
     root = root
   )
 
-  class(config_obj) <- CONSTANTS$CLASSES$API_CONFIG
+  class(config_obj) <- append(class(config_obj), CONSTANTS$CLASSES$API_CONFIG)
   return(config_obj)
 }
 
@@ -331,6 +332,43 @@ get_valid_statuses <- function() {
   return(CONSTANTS$JOB_STATUS)
 }
 
+
+#' get_job
+#'
+#' function to query an RSQ API and return the current status/info of single job.
+#' job can be returned as a list object with class `rsq_job`, or as a data.frame.
+#'
+#' @param api_config an api_config object
+#' @param job_id the numeric ID of the RSQ job to return, OR an object of class "rsq_job"
+#' @param return_df boolean. If TRUE, results will be returned as a one-row data.frame.
+#'   Otherwise, results will be returned as a list-object.
+get_job <- function(
+  api_config,
+  job,
+  return_df = FALSE
+) {
+  check_api_config(api_config)
+  if(inherits(job, CONSTANTS$CLASSES$RSQ_JOB)) {
+    job_id = job$id
+  } else {
+    job_id = job
+  }
+
+  endpoint_url <- get_endpoint_url(
+    api_config = api_config,
+    endpoint = glue::glue("{CONSTANTS$ENDPOINTS$JOB}/{job_id}")
+  )
+
+  api_results <- api_get(endpoint_url = endpoint_url)
+  class(api_results) <- append(class(api_results), CONSTANTS$CLASSES$RSQ_JOB)
+
+  if(return_df) {
+    return(api_results %>% job_to_df())
+  } else {
+    return(api_results)
+  }
+}
+
 #' get_jobs
 #'
 #' function to query an RSQ API and return a data.frame of all jobs
@@ -361,7 +399,7 @@ get_jobs <- function(
 
   # A bit of a conundrum.
   # I want to be let users query multiple statuses at once. Do I make up to four small API calls, but usually only one or two?
-  # or do I always make a big API call and then filter down later?
+  # Or do I always make a big API call and then filter down later?
   # Answer for now: Since RSQ has always been hosted locally (thus far), we can expect there to be minimal latency
   # between the client and server. Therefore, I think it is reasonable to make multiple small requests, as the overhead is
   # likely to be a bit lower in practice.
@@ -379,5 +417,64 @@ get_jobs <- function(
   }
 
   return(jobs_df)
+}
+
+#' submit job2
+#'
+#' submit a job to RSQ using an rsq_api_config object.
+#' .submit_job is already a function, so we have added a "2" to this to avoid confusion.
+#'
+#' @param api_config an rsq_api_config object
+#' @param srvr rsq server url
+#' @param user user submitting job
+#' @param context job context
+#' @param rscript_path path to rscript
+#' @param work_dir directory to start R from
+#' @param r_path path to R executable
+#' @param renv list of environment variables
+#' @return
+#' an `rsq_job` object.
+submit_job2 <- function(
+  api_config,
+  user,
+  context,
+  rscript_path,
+  work_dir = NULL,
+  r_path = NULL,
+  renv = NULL
+) {
+  check_api_config(api_config)
+  endpoint_url <- get_endpoint_url(api_config, CONSTANTS$ENDPOINTS$JOB)
+
+  r_details <- unbox_details(collect_r_details())
+  if (!is.null(work_dir)) {
+    r_details$work_dir <- work_dir
+  }
+  if (!is.null(r_path)) {
+    r_details$r_path <- r_path
+  }
+  if (!is.null(renv)) {
+    r_details$renv <- renv
+  }
+  submission_values <- list(
+    "id"= jsonlite::unbox(0),
+    "status"= jsonlite::unbox("QUEUED"),
+    "user"= jsonlite::unbox(user),
+    "context"= jsonlite::unbox(context),
+    "rscript"= list(
+      "r_path"= jsonlite::unbox(r_details$r_path),
+      "work_dir"= jsonlite::unbox(r_details$work_dir),
+      "rscript_path"= jsonlite::unbox(rscript_path),
+      "renv"= r_details$renv
+    )
+  )
+
+  job <- api_post(
+    endpoint_url = endpoint_url,
+    body = submission_values
+  )
+
+  class(job) <- append(class(job), CONSTANTS$CLASSES$RSQ_JOB)
+  return(job)
 }
 
